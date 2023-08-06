ae15890eaa42de4602edbc48e0c76fc1e6ddf632
Adarsh Malviya <75481003+adarshmalviy@users.noreply.github.com>
2023-08-06 12:03:51 +0530

    Debug active

--- flight_booking/settings.py
+++ flight_booking/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-p2crh1#ua(dt_8%6&97@a)s@=9aj6p#h^cxkcr#!z3_!hf9$zx"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["flight-booking-red.vercel.app", ".vercel.app",'localhost', '127.0.0.1']
 
